72fb291e65d8ba367baa7b00df61a9229494c3e8
matthew jording <mjording@opengotham.com>
2010-09-05 15:54:21 -0400

    templates for step definitions

--- features/step_definitions/food_steps.rb
+++ features/step_definitions/food_steps.rb
@@ -1,3 +1,19 @@
 Given /^a menu$/ do
   @menu = Menu.new
 end
+
+When /^I add a menu item$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^a description$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^a price$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^bowlers can see the item on the menu$/ do
+  pending # express the regexp above with the code you wish you had
+end
